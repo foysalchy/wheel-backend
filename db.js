fbea3f70c925b68316a27718633b8aae5845f0f6
Foysal Chowdhury <foysal.chy2801@gmail.com>
2026-05-03 17:24:38 +0600

    update

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "bhaiya.digital",
+  host: "103.163.246.85",
   user: "finixcom_wheel",
   password: "finixcom_wheel",
   database: "wheel",
